9067c4ce2b02b8fa81c03761f0924bd4e311e9fd
Fabrice CARUSO <fabrice.caruso@free.fr>
2020-05-16 19:29:12 +0200

    Demul : Push Alt+Enter to make it fullscreen

--- emulatorLauncher/Generators/Demul.Generator.cs
+++ emulatorLauncher/Generators/Demul.Generator.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Text;
 using System.IO;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
+using emulatorLauncher.PadToKeyboard;
+using System.Windows.Forms;
+using System.Threading;
 
 namespace emulatorLauncher
 {
@@ -53,10 +57,95 @@ namespace emulatorLauncher
             return new ProcessStartInfo()
             {
                 FileName = exe,
-                WorkingDirectory = path,
+                WorkingDirectory = path,                
                 Arguments = "-run=" + demulCore + " -rom=\"" + Path.GetFileNameWithoutExtension(rom).ToLower() + "\"",
             };
         }
+
+        public override void RunAndWait(ProcessStartInfo path)
+        {
+            var process = Process.Start(path);
+
+            while (process != null)
+            {
+                if (process.WaitForExit(50))
+                {
+                    process = null;
+                    break;
+                }
+
+                var hWnd = FindHwnd(process.Id);
+                if (hWnd == IntPtr.Zero)
+                    continue;
+                
+                var name = GetWindowText(hWnd);
+                if (name != null && name.StartsWith("gpu"))
+                {                    
+                    SendKeys.SendWait("%~");
+                    break;
+                }
+            }
+
+            if (process != null)
+                process.WaitForExit();
+        }
+
+        #region Apis
+        private IntPtr FindHwnd(int processId)
+        {
+            IntPtr hWnd = GetWindow(GetDesktopWindow(), GW.CHILD);
+            while (hWnd != IntPtr.Zero)
+            {
+                if (IsWindowVisible(hWnd))
+                {
+                    uint wndProcessId;
+                    GetWindowThreadProcessId(hWnd, out wndProcessId);
+                    if (wndProcessId == processId)
+                        return hWnd;
+                }
+
+                hWnd = GetWindow(hWnd, GW.HWNDNEXT);
+            }
+
+            return IntPtr.Zero;
+        }
+
+        [DllImport("user32.dll", SetLastError = false)]
+        public static extern IntPtr GetDesktopWindow();
+
+        [DllImport("user32.dll")]
+        public static extern bool IsWindowVisible(IntPtr hWnd);
+
+        [DllImport("User32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr GetWindow(IntPtr hWnd, GW cmd);
+
+        public enum GW : int
+        {
+            HWNDFIRST = 0,
+            HWNDLAST = 1,
+            HWNDNEXT = 2,
+            HWNDPREV = 3,
+            OWNER = 4,
+            CHILD = 5
+        }
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern int GetWindowTextLength(IntPtr hWnd);
+
+        public static string GetWindowText(IntPtr hWnd)
+        {
+            int capacity = GetWindowTextLength(hWnd) * 2;
+            StringBuilder lpString = new StringBuilder(capacity);
+            GetWindowText(hWnd, lpString, lpString.Capacity);
+            return lpString.ToString();
+        }
+        #endregion
 
         private void SetupGeneralConfig(string path, string rom, string system)
         {
@@ -100,7 +189,7 @@ namespace emulatorLauncher
             {
                 using (var ini = new IniFile(iniFile, true))
                 {
-                    ini.WriteValue("main", "UseFullscreen", "1");
+                    ini.WriteValue("main", "UseFullscreen", "0");
                     ini.WriteValue("main", "Vsync", SystemConfig["VSync"] != "false" ? "1" : "0");                    
                 }
             }
