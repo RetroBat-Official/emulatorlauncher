9d19137f43dfd528fe1fdc3ec8c859349e072c19
Fabrice CARUSO <fabrice.caruso@free.fr>
2020-06-20 11:42:30 +0200

    pcsx2 features support

--- emulatorLauncher/Generators/Pcsx2.Generator.cs
+++ emulatorLauncher/Generators/Pcsx2.Generator.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.IO;
 using System.Diagnostics;
+using System.Windows.Forms;
 
 namespace emulatorLauncher
 {
@@ -14,21 +15,21 @@ namespace emulatorLauncher
             DependsOnDesktopResolution = true;
         }
 
-        private string path;
-        private string romName;
-
-        private const string savDirName = "tmp";
+        private string _path;
 
         public override System.Diagnostics.ProcessStartInfo Generate(string system, string emulator, string core, string rom, string playersControllers, ScreenResolution resolution)
         {
-            path = AppConfig.GetFullPath("pcsx2");
+            _path = AppConfig.GetFullPath("pcsx2");
 
-            string exe = Path.Combine(path, "pcsx2.exe");
+            string exe = Path.Combine(_path, "pcsx2.exe");
             if (!File.Exists(exe))
                 return null;
 
             SetupPaths();
+            SetupVM();
+            SetupGSDx(resolution);
 
+            /*
             romName = Path.GetFileNameWithoutExtension(rom);
 
             RestoreIni(path, null, "GSdx.ini", true);
@@ -36,25 +37,36 @@ namespace emulatorLauncher
 
             SaveIni(path, romName, "GSdx.ini");
             SaveIni(path, romName, "PCSX2_vm.ini");
-         
+            */
+
+            List<string> commandArray = new List<string>();
+            commandArray.Add("--portable");
+            commandArray.Add("--fullscreen");
+            commandArray.Add("--nogui");
+
+            if (SystemConfig.isOptSet("fullboot") && SystemConfig.getOptBoolean("fullboot"))
+                commandArray.Add("--fullboot");
+
+            string args = string.Join(" ", commandArray);
+
             return new ProcessStartInfo()
             {
                 FileName = exe,
-                WorkingDirectory = path,
-                Arguments = "--portable --fullscreen --nogui \"" + rom + "\"", 
+                WorkingDirectory = _path,
+                Arguments = args + " \"" + rom + "\"", 
             };
         }
 
         private void SetupPaths()
         {
-            string iniFile = Path.Combine(path, "inis", "PCSX2_ui.ini");
+            string iniFile = Path.Combine(_path, "inis", "PCSX2_ui.ini");
             if (File.Exists(iniFile))
             {
                 try
                 {
                     using (var ini = new IniFile(iniFile))
                     {
-                        Uri relRoot = new Uri(path, UriKind.Absolute);
+                        Uri relRoot = new Uri(_path, UriKind.Absolute);
 
                         string biosPath = AppConfig.GetFullPath("bios");
                         if (!string.IsNullOrEmpty(biosPath))
@@ -79,6 +91,87 @@ namespace emulatorLauncher
             }
         }
 
+        private void SetupVM()
+        {
+            string iniFile = Path.Combine(_path, "inis", "PCSX2_vm.ini");
+            if (File.Exists(iniFile))
+            {
+                try
+                {
+                    using (var ini = new IniFile(iniFile))
+                    {
+                        string negdivhack = Program.AppConfig["negdivhack"] == "1" ? "enabled" : "disabled";
+
+                        ini.WriteValue("EmuCore/CPU/Recompiler", "vuExtraOverflow", negdivhack);
+                        ini.WriteValue("EmuCore/CPU/Recompiler", "vuSignOverflow", negdivhack);
+                        ini.WriteValue("EmuCore/CPU/Recompiler", "fpuExtraOverflow", negdivhack);
+                        ini.WriteValue("EmuCore/CPU/Recompiler", "fpuFullMode", negdivhack);
+
+                        ini.WriteValue("EmuCore/Gamefixes", "VuClipFlagHack", negdivhack);
+                        ini.WriteValue("EmuCore/Gamefixes", "FpuNegDivHack", negdivhack);
+                    }
+                }
+                catch { }
+            }
+        }
+                        
+        private void SetupGSDx(ScreenResolution resolution)
+        {
+            string iniFile = Path.Combine(_path, "inis", "GSdx.ini");
+            if (File.Exists(iniFile))
+            {
+                try
+                {
+                    using (var ini = new IniFile(iniFile))
+                    {
+                        if (!string.IsNullOrEmpty(SystemConfig["internalresolution"]))
+                            ini.WriteValue("Settings", "upscale_multiplier", SystemConfig["internalresolution"]);
+                        else
+                            ini.WriteValue("Settings", "upscale_multiplier", "0");
+
+                        if (string.IsNullOrEmpty(SystemConfig["internalresolution"]) || SystemConfig["internalresolution"] == "0")
+                        {
+                            if (resolution != null)
+                            {
+                                ini.WriteValue("Settings", "resx", resolution.Width.ToString());
+                                ini.WriteValue("Settings", "resy", (resolution.Height * 2).ToString());
+                            }
+                            else
+                            {
+                                ini.WriteValue("Settings", "resx", Screen.PrimaryScreen.Bounds.Width.ToString());
+                                ini.WriteValue("Settings", "resy", (Screen.PrimaryScreen.Bounds.Height * 2).ToString());
+                            }
+                        }
+
+                        if (SystemConfig.isOptSet("interlace") && !string.IsNullOrEmpty(SystemConfig["interlace"]))
+                            ini.WriteValue("Settings", "interlace", SystemConfig["interlace"]);
+                        else
+                            ini.WriteValue("Settings", "interlace", "7");
+
+
+
+                        if (SystemConfig.isOptSet("DrawFramerate") && SystemConfig.getOptBoolean("DrawFramerate"))
+                        {
+                            ini.WriteValue("Settings", "osd_monitor_enabled", "1");
+                            ini.WriteValue("Settings", "osd_indicator_enabled", "1");
+                        }
+                        else
+                        {
+                            ini.WriteValue("Settings", "osd_monitor_enabled", "0");
+                            ini.WriteValue("Settings", "osd_indicator_enabled", "0");
+                        }
+                    }
+
+                }
+                catch { }
+            }
+        }
+
+
+        /*
+        private string romName;
+        private const string savDirName = "tmp";
+        
         public override void Cleanup()
         {
             RestoreIni(path, romName, "GSdx.ini");
@@ -92,7 +185,7 @@ namespace emulatorLauncher
             }
             catch { }
         }
-
+     
         static void SaveIni(string path, string romName, string iniName)
         {
             string ini = Path.Combine(path, "inis", romName, iniName);
@@ -143,6 +236,6 @@ namespace emulatorLauncher
 
                 }
             }
-        }
+        }   */
     }
 }
