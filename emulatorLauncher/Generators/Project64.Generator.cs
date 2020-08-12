e93ca305384cf5e2e56e7a6cd83fe90b16d78b9b
Kayl <adriencd@hotmail.fr>
2020-08-12 20:57:50 +0200

    Create Project64.Generator.cs

---
+++ emulatorLauncher/Generators/Project64.Generator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
+
+namespace emulatorLauncher
+{
+    class Project64Generator : Generator
+    {
+        public override System.Diagnostics.ProcessStartInfo Generate(string system, string emulator, string core, string rom, string playersControllers, ScreenResolution resolution)
+        {
+            string path = AppConfig.GetFullPath("project64");
+
+            string exe = Path.Combine(path, "Project64.exe");
+            if (!File.Exists(exe))
+                return null;
+
+            return new ProcessStartInfo()
+            {
+                FileName = exe,
+                WorkingDirectory = path,
+                Arguments = "\"" + rom + "\"",
+            };
+        }
+    }
+}
