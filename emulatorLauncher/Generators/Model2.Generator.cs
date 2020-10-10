c59db20acf1ad248abc291bb8f53f8a6b9958ede
Fabrice CARUSO <fabrice.caruso@free.fr>
2020-10-10 21:04:56 +0200

    Model2 : support for parent rom

--- emulatorLauncher/Generators/Model2.Generator.cs
+++ emulatorLauncher/Generators/Model2.Generator.cs
@@ -10,12 +10,70 @@ namespace emulatorLauncher
 {
     class Model2Generator : Generator
     {
+        static Dictionary<string, string> parentRoms = new Dictionary<string, string>() 
+        { 
+            // Daytona USA
+            { "dayton93", "daytona" },
+            { "daytonas", "daytona" },
+            { "daytonase", "daytona" },
+            { "daytonat", "daytona" },
+            { "daytonata", "daytona" },
+            { "daytonagtx", "daytona" },
+            { "daytonam", "daytona" },
+            // Dead Or Alive
+            { "doaa", "doa" },
+            // Dynamite Cop
+            { "dynmcopb", "dynamcop" },
+            { "dynmcopc", "dynamcop" },
+            // Dynamite Deka 
+            { "dyndeka2", "dynamcop" },
+            { "dyndek2b", "dynamcop" },
+            // Indianapolis 500
+            { "indy500d", "indy500" },
+            { "indy500to", "indy500" },
+            // Last Bronx
+            { "lastbrnxj", "lastbrnx" },
+            { "lastbrnxu", "lastbrnx" },
+            // Pilot Kids
+            { "pltkidsa", "pltkids" },            
+            // Over Rev
+            { "overrevb", "overrev" }, 
+            // Sega Rally Championship
+            { "srallycb", "srallyc" },
+            { "srallyp", "srallyc" },
+            // Sega Touring Car Championship
+            { "stcca", "stcc" },
+            { "stccb", "stcc" },
+            // Top Skater
+            { "topskatrj", "topskatr" },
+            { "topskatru", "topskatr" },
+            // Sonic The Fighters
+            { "sfight", "schamp" },            
+            // Virtua Cop
+            { "vcopa", "vcop" },            
+            // Virtua Fighter 2
+            { "vf2o", "vf2" },
+            { "vf2a", "vf2" },
+            { "vf2b", "vf2" },
+            // Virtua Striker
+            { "vstrikro", "vstriker" },
+            // Virtual On Cybertroopers
+            { "vonj", "von" },
+            // Zero Gunner
+            { "zerogunaj", "zerogun" },
+            { "zerogunj", "zerogun" },
+            { "zeroguna", "zerogun" },
+        };
+ 
+
+
         public Model2Generator()
         {
             DependsOnDesktopResolution = false;
         }
 
         private string _destFile;
+        private string _destParent;
 
         public override System.Diagnostics.ProcessStartInfo Generate(string system, string emulator, string core, string rom, string playersControllers, ScreenResolution resolution)
         {
@@ -42,7 +100,28 @@ namespace emulatorLauncher
 
             _destFile = Path.Combine(pakDir, Path.GetFileName(rom));
             if (!File.Exists(_destFile))
+            {
                 File.Copy(rom, _destFile);
+                
+                try { new FileInfo(_destFile).Attributes &= ~FileAttributes.ReadOnly; }
+                catch { }
+            }
+
+            string parentRom = null;
+            if (parentRoms.TryGetValue(Path.GetFileNameWithoutExtension(rom).ToLowerInvariant(), out parentRom))
+            {
+                parentRom = Path.Combine(Path.GetDirectoryName(rom), parentRom + ".zip");
+                _destParent = Path.Combine(pakDir, Path.GetFileName(parentRom));
+
+                if (!File.Exists(_destParent))
+                {
+                    File.Copy(parentRom, _destParent);
+
+                    try { new FileInfo(_destParent).Attributes &= ~FileAttributes.ReadOnly; }
+                    catch { }
+                }
+
+            }
             
             SetupConfig(path, resolution);
             
@@ -79,6 +158,9 @@ namespace emulatorLauncher
         {
             if (_destFile != null && File.Exists(_destFile))
                 File.Delete(_destFile);
+
+            if (_destParent != null && File.Exists(_destParent))
+                File.Delete(_destParent);            
         }
     }
 }
