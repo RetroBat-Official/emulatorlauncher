af645da4c98b9bcbaf3de7533023f48433cf13da
Kayl <adriencd@hotmail.fr>
2021-02-02 23:44:49 +0100

    Create Pico8.Generator.cs
    Run Pico8 games from source files (png, p8) with Pico-8 engine. Can run also compiled games (exe) without the need of Pico-8..

---
+++ emulatorLauncher/Generators/Pico8.Generator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
+
+namespace emulatorLauncher
+{
+    class Pico8Generator : Generator
+    {
+        public override System.Diagnostics.ProcessStartInfo Generate(string system, string emulator, string core, string rom, string playersControllers, ScreenResolution resolution)
+        {
+            string path = AppConfig.GetFullPath("pico8");
+
+            string exe = Path.Combine(path, "pico8.exe");
+            if (!File.Exists(exe))
+                return null;
+				
+			if (Path.GetExtension(rom).ToLower() == ".exe")
+			{
+				path = Path.GetDirectoryName(rom);
+				
+				if (!File.Exists(rom))
+                return null;
+
+                return new ProcessStartInfo()
+                {
+					FileName = rom,
+					WorkingDirectory = path
+                };
+
+			}
+
+            return new ProcessStartInfo()
+            {
+                FileName = exe,
+                WorkingDirectory = path,
+                Arguments = "-run -windowed 0 \"" + rom + "\"",
+            };
+        }
+    }
+}
