b5077f29636e83ea73ced93a47495ba596e1e35a
Benoit Schneider <tartifless@gmail.com>
2022-12-07 17:27:35 +0100

    Update Pcsx2.Generator.cs
    PCSX2-QT will be in PCSX2 path, not in separate path, change in code

--- emulatorLauncher/Generators/Pcsx2.Generator.cs
+++ emulatorLauncher/Generators/Pcsx2.Generator.cs
@@ -48,10 +48,6 @@ namespace emulatorLauncher
 
             _path = AppConfig.GetFullPath(folderName);
 
-            //If path does not exist try pcsx2 1.7 path
-            if (string.IsNullOrEmpty(_path))
-                _path = AppConfig.GetFullPath("pcsx2");
-
             //If path does not exist try pcsx2 1.6 path
             if (string.IsNullOrEmpty(_path))
                 _path = AppConfig.GetFullPath("pcsx2-16");
@@ -69,7 +65,7 @@ namespace emulatorLauncher
 
             // v1.7.0 ???
             Version version = new Version();
-            if (Version.TryParse(FileVersionInfo.GetVersionInfo(exe).ProductVersion, out version))
+            if (!_isPcsxqt && Version.TryParse(FileVersionInfo.GetVersionInfo(exe).ProductVersion, out version))
                 _isPcsx17 = version >= new Version(1, 7, 0, 0);
 
             // Select avx2 build for qt version
