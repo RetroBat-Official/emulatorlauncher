31c64e028ecb15056fdb28a010942de2ea68244a
Benoit Schneider <tartifless@gmail.com>
2023-04-11 10:23:40 +0200

    FBNEO - disable patched roms if hardcore mode is enabled

--- emulatorLauncher/Generators/LibRetro.CoreOptions.cs
+++ emulatorLauncher/Generators/LibRetro.CoreOptions.cs
@@ -968,7 +968,12 @@ namespace emulatorLauncher.libRetro
                 return;
 
             coreSettings["fbneo-allow-depth-32"] = "enabled";
-            coreSettings["fbneo-allow-patched-romsets"] = "enabled";
+
+            if (Features.IsSupported("cheevos") && SystemConfig.getOptBoolean("retroachievements") && SystemConfig.getOptBoolean("retroachievements.hardcore"))
+                coreSettings["fbneo-allow-patched-romsets"] = "disabled";
+            else
+                coreSettings["fbneo-allow-patched-romsets"] = "enabled";
+
             coreSettings["fbneo-memcard-mode"] = "per-game";
             coreSettings["fbneo-hiscores"] = "enabled";
             coreSettings["fbneo-load-subsystem-from-parent"] = "enabled";
