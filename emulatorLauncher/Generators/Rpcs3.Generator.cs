ff866d3ea40ff9737df50aceffb5dd37af0911a6
Benoit Schneider <tartifless@gmail.com>
2023-06-06 09:17:46 +0200

    RPCS3 - add Force CPU blit for Gran Turismo games

--- emulatorLauncher/Generators/Rpcs3.Generator.cs
+++ emulatorLauncher/Generators/Rpcs3.Generator.cs
@@ -166,6 +166,7 @@ namespace emulatorLauncher
             BindFeature(video, "Anisotropic Filter Override", "anisotropicfilter", "0");
             BindFeature(video, "Shader Precision", "shader_quality", "Auto");
             BindFeature(video, "Driver Wake-Up Delay", "driver_wake", "1");
+            BindBoolFeature(video, "Force CPU Blit", "cpu_blit", "true", "false");
 
             // ZCULL Accuracy
             if (SystemConfig.isOptSet("zcull_accuracy") && (SystemConfig["zcull_accuracy"] == "Approximate"))
