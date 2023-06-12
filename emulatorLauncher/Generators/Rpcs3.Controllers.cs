995ed5327ae930972bb92616e3e2a88f9c77cdf3
Benoit Schneider <tartifless@gmail.com>
2023-06-12 18:27:37 +0200

    RPCS3: fix nsamepad and move controllers to SDL instead of MM

--- emulatorLauncher/Generators/Rpcs3.Controllers.cs
+++ emulatorLauncher/Generators/Rpcs3.Controllers.cs
@@ -32,9 +32,12 @@ namespace emulatorLauncher
 
             var yml = YmlFile.Load(controllerSettings);
 
+            Dictionary<string, int> double_pads = new Dictionary<string, int>();
+            int nsamepad = 1;
+
             //Create a single Player block in the file for each player
             foreach (var controller in this.Controllers.OrderBy(i => i.PlayerIndex))
-                ConfigureInput(yml, controller);
+                ConfigureInput(yml, controller, double_pads, nsamepad);
 
             // Save to yml file
             yml.Save();
@@ -46,7 +49,7 @@ namespace emulatorLauncher
         /// <param name="controllerSettings"></param>
         /// <param name="controller"></param>
         /// <param name="yml"></param>
-        private void ConfigureInput(YmlContainer yml, Controller controller)
+        private void ConfigureInput(YmlContainer yml, Controller controller, Dictionary<string, int> double_pads, int nsamepad)
         {
             if (controller == null || controller.Config == null)
                 return;
@@ -54,7 +57,7 @@ namespace emulatorLauncher
             if (controller.IsKeyboard)
                 ConfigureKeyboard(yml, controller.Config);
             else
-                ConfigureJoystick(yml, controller, controller.PlayerIndex);
+                ConfigureJoystick(yml, controller, controller.PlayerIndex, double_pads, nsamepad);
         }
 
         /// <summary>
@@ -177,7 +180,7 @@ namespace emulatorLauncher
         /// <param name="ctrl"></param>
         /// <param name="playerIndex"></param>
         /// <param name="yml"></param>
-        private void ConfigureJoystick(YmlContainer yml, Controller ctrl, int playerIndex)
+        private void ConfigureJoystick(YmlContainer yml, Controller ctrl, int playerIndex, Dictionary<string, int> double_pads, int nsamepad)
         {
             if (ctrl == null)
                 return;
@@ -192,7 +195,7 @@ namespace emulatorLauncher
             var prod = ctrl.ProductID;
 
             //define type of controller
-            string tech = "MMJoystick";
+            string tech = "SDL";
             if (prod == USB_PRODUCT.SONY_DS5)
                 tech = "DualSense";
             else if (prod == USB_PRODUCT.SONY_DS4 || prod == USB_PRODUCT.SONY_DS4_DONGLE || prod == USB_PRODUCT.SONY_DS4_SLIM)
@@ -206,7 +209,7 @@ namespace emulatorLauncher
             string playerBlockTitle = "Player" + " " + playerIndex + " " + "Input";
             var player = yml.GetOrCreateContainer(playerBlockTitle);
 
-            //set controller handler : use specific Dualshock handler for Dualshocks, XInput for XInput controllers, MMJoystick for all other cases
+            //set controller handler : use specific Dualshock handler for Dualshocks, XInput for XInput controllers, SDL for all other cases
             if (tech == "DualSense")
                 player["Handler"] = "DualSense";
             else if (tech == "DS4")
@@ -216,13 +219,9 @@ namespace emulatorLauncher
             else if (tech == "XInput")
                 player["Handler"] = "XInput";
             else
-                player["Handler"] = "MMJoystick";
+                player["Handler"] = "SDL";
 
-            //Set device & index (incremental for Dualshocks and XInput, actual device index for MMJoysticks)
-            int device = -1;
-            int nsamepad = 1;
-            
-            Dictionary<string, int> double_pads = new Dictionary<string, int>();
+            //Set device & index (incremental for Dualshocks and XInput, actual device index for SDL)
             
             if (double_pads.ContainsKey(tech + "/" + devicename))
                 nsamepad = double_pads[tech + "/" + devicename];
@@ -235,16 +234,11 @@ namespace emulatorLauncher
                 player["Device"] = "\"" + tech + " Pad #" + nsamepad + "\"";
             else if (tech == "XInput")
             {
-                int index = ctrl.XInput.DeviceIndex;
-                device = index + 1;
-                player["Device"] = "\"" + "XInput Pad #" + device + "\"";
+                int index = ctrl.XInput.DeviceIndex + 1;
+                player["Device"] = "\"" + "XInput Pad #" + index + "\"";
             }
             else
-            {
-                int index = ctrl.WinmmJoystick.Index;
-                device = index + 1;
-                player["Device"] = "\"" + "Joystick #" + device + "\"";
-            }
+                player["Device"] = devicename + " " + nsamepad;
 
             //config part
             var config = player.GetOrCreateContainer("Config");
@@ -307,7 +301,37 @@ namespace emulatorLauncher
                 config["L3"] = GetInputKeyNameX(ctrl, InputKey.l3, tech);                            //LS
             }
 
-            if (tech == "MMJoystick")
+            else if (tech == "SDL")
+            {
+                config["Left Stick Left"] = GetInputKeyNameSDL(ctrl, InputKey.joystick1left, tech);    //LS X-
+                config["Left Stick Down"] = GetInputKeyNameSDL(ctrl, InputKey.joystick1down, tech);    //LS Y-
+                config["Left Stick Right"] = GetInputKeyNameSDL(ctrl, InputKey.joystick1right, tech);  //LS X+
+                config["Left Stick Up"] = GetInputKeyNameSDL(ctrl, InputKey.joystick1up, tech);        //LS Y+
+                config["Right Stick Left"] = GetInputKeyNameSDL(ctrl, InputKey.joystick2left, tech);   //RS X-
+                config["Right Stick Down"] = GetInputKeyNameSDL(ctrl, InputKey.joystick2down, tech);   //LS Y-
+                config["Right Stick Right"] = GetInputKeyNameSDL(ctrl, InputKey.joystick2right, tech); //LS X+
+                config["Right Stick Up"] = GetInputKeyNameSDL(ctrl, InputKey.joystick2up, tech);       //LS Y+
+                config["Start"] = GetInputKeyNameSDL(ctrl, InputKey.start, tech);                      //Start
+                config["Select"] = GetInputKeyNameSDL(ctrl, InputKey.select, tech);                    //Back
+                config["PS Button"] = "Guide";                                                         //Guide
+                config["Square"] = GetInputKeyNameSDL(ctrl, InputKey.x, tech);                         //X (or Y on nintendo)
+                config["Cross"] = GetInputKeyNameSDL(ctrl, InputKey.a, tech);                          //A (or B on nintendo)
+                config["Circle"] = GetInputKeyNameSDL(ctrl, InputKey.b, tech);                         //B (or A on nintendo)
+                config["Triangle"] = GetInputKeyNameSDL(ctrl, InputKey.y, tech);                       //Y (or X on nintendo)
+                config["Left"] = GetInputKeyNameSDL(ctrl, InputKey.left, tech);                        //Left
+                config["Down"] = GetInputKeyNameSDL(ctrl, InputKey.down, tech);                        //Down
+                config["Right"] = GetInputKeyNameSDL(ctrl, InputKey.right, tech);                      //Right
+                config["Up"] = GetInputKeyNameSDL(ctrl, InputKey.up, tech);                            //Up
+                config["R1"] = GetInputKeyNameSDL(ctrl, InputKey.r1, tech);                            //RB
+                config["R2"] = GetInputKeyNameSDL(ctrl, InputKey.r2, tech);                            //RT
+                config["R3"] = GetInputKeyNameSDL(ctrl, InputKey.r3, tech);                            //RS
+                config["L1"] = GetInputKeyNameSDL(ctrl, InputKey.l1, tech);                            //LB
+                config["L2"] = GetInputKeyNameSDL(ctrl, InputKey.l2, tech);                            //LT
+                config["L3"] = GetInputKeyNameSDL(ctrl, InputKey.l3, tech);                            //LS
+            }
+
+            // Previous MM part before RPCS3 was compatible with SDL
+            /*else if (tech == "MMJoystick")
             {
                 config["Left Stick Left"] = GetInputKeyNameMM(ctrl, InputKey.joystick1left, tech);    //X-
                 config["Left Stick Down"] = GetInputKeyNameMM(ctrl, InputKey.joystick1down, tech);    //Y+
@@ -337,7 +361,7 @@ namespace emulatorLauncher
                 config["L1"] = GetInputKeyNameMM(ctrl, InputKey.l1, tech);                            //Button 5
                 config["L2"] = GetInputKeyNameMM(ctrl, InputKey.l2, tech);                            //Button 7
                 config["L3"] = GetInputKeyNameMM(ctrl, InputKey.l3, tech);                            //Button 11
-            }
+            }*/
             
             //motion controls
             var motionx = config.GetOrCreateContainer("Motion Sensor X");
@@ -363,13 +387,13 @@ namespace emulatorLauncher
             config["Left Stick Multiplier"] = "100";
             config["Right Stick Multiplier"] = "100";
             if (tech == "XInput")
-                config["Left Stick Deadzone"] = "7849";
+                config["Left Stick Deadzone"] = "7849";         //Maybe add a feature
             else
-                config["Left Stick Deadzone"] = "50";
+                config["Left Stick Deadzone"] = "50";           //Maybe add a feature
             if (tech == "XInput")
-                config["Right Stick Deadzone"] = "8689";
+                config["Right Stick Deadzone"] = "8689";        //Maybe add a feature
             else
-                config["Right Stick Deadzone"] = "50";
+                config["Right Stick Deadzone"] = "50";          //Maybe add a feature
             if (tech == "XInput")
                 config["Left Trigger Threshold"] = "30";
             else
@@ -393,14 +417,14 @@ namespace emulatorLauncher
             else
                 config["LED battery indicator brightness"] = "50";
             config["Player LED enabled"] = "true";
-            config["Enable Large Vibration Motor"] = "true";
-            config["Enable Small Vibration Motor"] = "true";
+            config["Enable Large Vibration Motor"] = "true";    //Maybe add a feature
+            config["Enable Small Vibration Motor"] = "true";    //Maybe add a feature
             config["Switch Vibration Motors"] = "false";
             config["Mouse Movement Mode"] = "Relative";
-            config["Mouse Deadzone X Axis"] = "60";
-            config["Mouse Deadzone Y Axis"] = "60";
-            config["Mouse Acceleration X Axis"] = "200";
-            config["Mouse Acceleration Y Axis"] = "250";
+            config["Mouse Deadzone X Axis"] = "60";             //Maybe add a feature when managing guns in the future
+            config["Mouse Deadzone Y Axis"] = "60";             //Maybe add a feature when managing guns in the future
+            config["Mouse Acceleration X Axis"] = "200";        //Maybe add a feature when managing guns in the future
+            config["Mouse Acceleration Y Axis"] = "250";        //Maybe add a feature when managing guns in the future
             config["Left Stick Lerp Factor"] = "100";
             config["Right Stick Lerp Factor"] = "100";
             config["Analog Button Lerp Factor"] = "100";
@@ -534,6 +558,80 @@ namespace emulatorLauncher
                 }
             }
                 return "\"\"";
+        }
+
+        private static string GetInputKeyNameSDL(Controller c, InputKey key, string tech)
+        {
+            Int64 pid = -1;
+
+            // If controller is nintendo, A/B and X/Y are reversed
+            bool revertbuttons = (c.VendorID == USB_VENDOR.NINTENDO) || (Program.SystemConfig.isOptSet("rpcs3_gamepadbuttons") && Program.SystemConfig.getOptBoolean("rpcs3_gamepadbuttons"));
+
+            bool revertAxis = false;
+            key = key.GetRevertedAxis(out revertAxis);
+
+            var input = c.Config[key];
+
+            if (input != null)
+            {
+                if (input.Type == "button")
+                {
+                    pid = input.Id;
+                    switch (pid)
+                    {
+                        case 0: return revertbuttons ? "A" : "B";
+                        case 1: return revertbuttons ? "B" : "A";
+                        case 2: return revertbuttons ? "Y" : "X";
+                        case 3: return revertbuttons ? "X" : "Y";
+                        case 4: return "Back";
+                        case 6: return "Start";
+                        case 7: return "LS";
+                        case 8: return "RS";
+                        case 9: return "LB";
+                        case 10: return "RB";
+                        case 11: return "Up";
+                        case 12: return "Down";
+                        case 13: return "Left";
+                        case 14: return "Right";
+                    }
+                }
+
+                if (input.Type == "axis")
+                {
+                    pid = input.Id;
+                    switch (pid)
+                    {
+                        case 0:
+                            if ((!revertAxis && input.Value > 0) || (revertAxis && input.Value < 0)) return "LS X+";
+                            else return "LS X-";
+                        case 1:
+                            if ((!revertAxis && input.Value > 0) || (revertAxis && input.Value < 0)) return "LS Y-";
+                            else return "LS Y+";
+                        case 2:
+                            if ((!revertAxis && input.Value > 0) || (revertAxis && input.Value < 0)) return "RS X+";
+                            else return "RS X-";
+                        case 3:
+                            if ((!revertAxis && input.Value > 0) || (revertAxis && input.Value < 0)) return "RS Y-";
+                            else return "RS Y+";
+                        case 4: return "LT";
+                        case 5: return "RT";
+                    }
+                }
+
+                if (input.Type == "hat")
+                {
+                    pid = input.Value;
+                    switch (pid)
+                    {
+                        case 1: return "Up";
+                        case 2: return "Right";
+                        case 4: return "Down";
+                        case 8: return "Left";
+                    }
+                }
+            }
+
+            return "\"\"";
         }
 
         private static string GetInputKeyNameMM(Controller c, InputKey key, string tech)
