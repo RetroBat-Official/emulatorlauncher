283a9a70aed27619c7987ee891b01deaacee3451
Benoit S <tartifless@gmail.com>
2023-08-16 01:41:46 +0200

    Kega fusion features

--- emulatorLauncher/Generators/KegaFusion.Generator.cs
+++ emulatorLauncher/Generators/KegaFusion.Generator.cs
@@ -26,6 +26,8 @@ namespace emulatorLauncher
             if (!File.Exists(exe))
                 return null;
 
+            SetupConfiguration(path);
+
             List<string> commandArray = new List<string>();
 
             if (core == "mastersystem" || core == "sms")
@@ -70,6 +72,50 @@ namespace emulatorLauncher
                 WorkingDirectory = path,
                 Arguments = args,
             };
+        }
+
+        private void SetupConfiguration(string path)
+        {
+            string iniFile = Path.Combine(path, "Fusion.ini");
+
+            using (var ini = IniFile.FromFile(iniFile))
+            {
+                // VIDEO
+                ini.WriteValue("", "FullScreen", "1");
+                ini.WriteValue("", "DFixedAspect", "1");
+                ini.WriteValue("", "DFixedZoom", "0");
+
+                BindIniFeature(ini, "", "VSyncEnabled", "vsync", "1");
+                BindIniFeature(ini, "", "DResolution", "kega_internal_resolution", "224,1,128,2");
+                BindIniFeature(ini, "", "DScanlines", "kega_scanlines", "3");
+                BindIniFeature(ini, "", "DRenderMode", "kega_rendermode", "0");
+                BindIniFeature(ini, "", "DFiltered", "kega_filter", "1");
+                BindIniFeature(ini, "", "Brighten", "kega_brighten", "1");
+                BindIniFeature(ini, "", "DNTSCAspect", "kega_ntsc_aspect", "1");
+                BindIniFeature(ini, "", "DNearestMultiple", "kega_nearest", "1");
+
+                if (SystemConfig.isOptSet("kega_scaler") && !string.IsNullOrEmpty(SystemConfig["kega_scaler"]))
+                {
+                    ini.WriteValue("", "DRenderMode", "3");
+                    ini.WriteValue("", "CurrentRenderPlugin", SystemConfig["kega_scaler"]);
+                }
+                else
+                    ini.WriteValue("", "DRenderMode", "0");
+
+                // OTHER
+                if (SystemConfig.isOptSet("kega_region") && !string.IsNullOrEmpty(SystemConfig["kega_region"]))
+                {
+                    ini.WriteValue("", "CurrentCountry", SystemConfig["kega_region"]);
+                    ini.WriteValue("", "CountryAutoDetect", "0");
+                }
+                else
+                {
+                    ini.WriteValue("", "CountryAutoDetect", "1");
+                }
+
+                BindIniFeature(ini, "", "FPSEnabled", "kega_fps", "0");
+
+            }
         }
 
         public override int RunAndWait(ProcessStartInfo path)
