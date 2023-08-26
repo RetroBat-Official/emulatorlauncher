3d0f41ea7e97565e58c495b77112f0b28049f6d3
Benoit S <tartifless@gmail.com>
2023-08-26 02:21:24 +0200

    Add bizhawk to emulatorlauncher
    Not yet added to RetroBat

---
+++ emulatorLauncher/Generators/Bizhawk.Guns.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Xml.Linq;
+using emulatorLauncher.Tools;
+
+namespace emulatorLauncher
+{
+    partial class BizhawkGenerator : Generator
+    {
+        // List of cores with dedicated configuration
+        // To be updated each time a core is correctly / succesfully configured
+        static List<string> coreGunConfig = new List<string>()
+        {
+            "xxx",
+        };
+
+        /// <summary>
+        /// Injects guns settings
+        /// </summary>
+        /// <param name="config"></param>
+        /// <param name="deviceType"></param>
+        /// <param name="playerIndex"></param>
+        private void SetupLightGuns(ConfigFile config, string deviceType, string core, int playerIndex = 1)
+        {
+            if (!SystemConfig.getOptBoolean("use_guns"))
+                return;
+        }
+
+        /// <summary>
+        /// Dedicated core mappings for lightgun games
+        private void ConfigureGunsCore(ConfigFile config, int playerIndex, string core, string deviceType, bool multigun = false, bool guninvert = false, bool useOneGun = false)
+        {
+         
+        }
+
+        private string GetcoreMouseButton (string core, bool guninvert, string mbtn)
+        {
+            string ret = "nul";
+
+            return ret;
+        }
+    }
+}
--- emulatorLauncher/Installer.cs
+++ emulatorLauncher/Installer.cs
@@ -82,7 +82,8 @@ namespace emulatorLauncher
             { new Installer("hbmame", "hbmame", "hbmameui.exe") },
             { new Installer("zinc", "zinc", "ZiNc.exe") },
             { new Installer("eduke32", "eduke32", "eduke32.exe") },
-            { new Installer("play", "play", "Play.exe") }
+            { new Installer("play", "play", "Play.exe") },
+            { new Installer("bizhawk", "bizhawk", "EmuHawk.exe") }
         };
 
         #region Properties
