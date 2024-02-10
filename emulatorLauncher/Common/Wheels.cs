6995785cd5111ee47efe832022e074b1fd090689
Benoit S <tartifless@gmail.com>
2024-02-10 15:34:26 +0100

    Add driving force GT wheel

--- emulatorLauncher/Common/Wheels.cs
+++ emulatorLauncher/Common/Wheels.cs
@@ -25,63 +25,6 @@ namespace EmulatorLauncher
 
                 else
                     return WheelType.Default;
-
-                /*string[] logitechDrivingForceids = new string[] { "VID_046D&PID_C294", "VID_046D&PID_C298" };
-                if (logitechDrivingForceids.Any(d => devicePath.Contains(d)))
-                    return WheelType.LogitechDrivingForce;                
-
-                string[] logitechG25ids = new string[] { "VID_046D&PID_C299" };
-                if (logitechG25ids.Any(d => devicePath.Contains(d)))
-                    return WheelType.LogitechG25;
-
-                string[] logitechG27ids = new string[] { "VID_046D&PID_C29B" };
-                if (logitechG27ids.Any(d => devicePath.Contains(d)))
-                    return WheelType.LogitechG27;
-
-                string[] logitechG29ids = new string[] { "VID_046D&PID_C24F" };
-                if (logitechG29ids.Any(d => devicePath.Contains(d)))
-                    return WheelType.LogitechG29;
-
-                string[] logitechG29altids = new string[] { "VID_046D&PID_C260" };
-                if (logitechG29ids.Any(d => devicePath.Contains(d)))
-                    return WheelType.LogitechG29alt;
-
-                string[] logitechG920ids = new string[] { "VID_046D&PID_C262" };
-                if (logitechG920ids.Any(d => devicePath.Contains(d)))
-                    return WheelType.LogitechG920;
-
-                string[] logitechG923xboxids = new string[] { "VID_046D&PID_C26E" };
-                if (logitechG923xboxids.Any(d => devicePath.Contains(d)))
-                    return WheelType.LogitechG923X;
-
-                string[] logitechG923PSids = new string[] { "VID_046D&PID_C266" };
-                if (logitechG923PSids.Any(d => devicePath.Contains(d)))
-                    return WheelType.LogitechG923PS;
-
-                string[] logitechMomoids = new string[] { "VID_046D&PID_CA03" };
-                if (logitechMomoids.Any(d => devicePath.Contains(d)))
-                    return WheelType.LogitechMomo;
-
-                string[] microsoftSideWinderids = new string[] { "VID_045E&PID_0034", "VID_045E&PID_001A" };
-                if (microsoftSideWinderids.Any(d => devicePath.Contains(d)))
-                    return WheelType.MicrosoftSideWinder;
-
-                string[] thrustmasterFerrariGTids = new string[] { "VID_044F&PID_B651", "VID_044F&PID_B654" };
-                if (thrustmasterFerrariGTids.Any(d => devicePath.Contains(d)))
-                    return WheelType.ThrustmasterFerrariGT;
-
-                string[] thrustmasterFFRacingids = new string[] { "VID_044F&PID_B605" };
-                if (thrustmasterFFRacingids.Any(d => devicePath.Contains(d)))
-                    return WheelType.ThrustmasterForceFeedbackRacing;
-
-                string[] thrustmasterRGTids = new string[] { "VID_044F&PID_B653" };
-                if (thrustmasterRGTids.Any(d => devicePath.Contains(d)))
-                    return WheelType.ThrustmasterRallyGT;
-
-                string[] thrustmasterT150ids = new string[] { "VID_044F&PID_B677" };
-                if (thrustmasterT150ids.Any(d => devicePath.Contains(d)))
-                    return WheelType.ThrustmasterT150;
-                */
             }
 
             else
@@ -115,8 +58,9 @@ namespace EmulatorLauncher
             { "VID_046D&PID_C266", WheelType.LogitechG923PS },          // G923 PS
             { "VID_046D&PID_C26E", WheelType.LogitechG923X },           // G923 Xbox
             { "VID_046D&PID_C294", WheelType.LogitechDrivingForce },    // Driving force
-            {"VID_046D&PID_C298", WheelType.LogitechDrivingForce },     // Driving Force
+            { "VID_046D&PID_C298", WheelType.LogitechDrivingForce },    // Driving Force
             { "VID_046D&PID_C299", WheelType.LogitechG25 },             // G25
+            { "VID_046D&PID_C29A", WheelType.LogitechDrivingForceGT },  // Driving Force GT
             { "VID_046D&PID_C29B", WheelType.LogitechG27 },             // G27
             { "VID_046D&PID_CA03", WheelType.LogitechMomo },            // Momo
             // Microsoft
@@ -139,6 +83,7 @@ namespace EmulatorLauncher
         LogitechG29alt,
         LogitechG920,
         LogitechG27,
+        LogitechDrivingForceGT
         LogitechG25,
         LogitechDrivingForce,
         ThrustmasterT150,
