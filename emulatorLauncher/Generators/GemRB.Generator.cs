fb8882384b9247f54cf1fe5c56cda8ff2983d8ae
Adrien Chalard <30243810+kaylh@users.noreply.github.com>
2024-03-27 01:40:00 +0100

    add gemrb engine compatiblity and its first features integration
    GemRB need the original games (not Enhanced Edition from Beamdog).
    They need to be copied in roms\gemrb and in a subfolder with an extension in its name. (e.g "roms\gemrb\Baldur's Gate.bg1").
    Then simply run the game from EmulationStation.

---
+++ emulatorLauncher/Generators/GemRB.Generator.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Diagnostics;
+using System.Threading;
+using EmulatorLauncher.Common;
+using EmulatorLauncher.Common.EmulationStation;
+using EmulatorLauncher.Common.FileFormats;
+using EmulatorLauncher.PadToKeyboard;
+using System.Windows.Forms;
+
+namespace EmulatorLauncher
+{
+    partial class GemRBGenerator : Generator
+    {
+        private ScreenResolution _resolution;
+
+        public override System.Diagnostics.ProcessStartInfo Generate(string system, string emulator, string core, string rom, string playersControllers, ScreenResolution resolution)
+        {
+            string path = AppConfig.GetFullPath("gemrb");
+
+            string exe = Path.Combine(path, "gemrb.exe");
+            if (!File.Exists(exe))
+                return null;
+
+            string conf = Path.Combine(path, "GemRB.cfg");
+            if (!File.Exists(exe))
+                return null;
+
+            bool fullscreen = !IsEmulationStationWindowed() || SystemConfig.getOptBoolean("forcefullscreen");
+
+            _resolution = resolution;
+
+            SetupConfiguration(path, rom, conf, system, fullscreen, resolution);
+
+            return new ProcessStartInfo()
+            {
+                FileName = exe,
+                WorkingDirectory = path,
+                Arguments = "-c \"" + conf + "\"",
+            };
+        }
+
+        private void SetupConfiguration(string path, string rom, string conf, string system, bool fullscreen, ScreenResolution resolution)
+        {
+            string gamePath = AppConfig.GetFullPath(rom);          
+            string gameExtension = Path.GetExtension(rom).Replace(".", "");
+            string gameName = new DirectoryInfo(gamePath).Name.Replace("." + gameExtension, "");
+
+            string savesPath = Path.Combine(AppConfig.GetFullPath("saves"), system, gameName);
+            if (!Directory.Exists(savesPath)) try { Directory.CreateDirectory(savesPath); }
+                catch { }
+
+            using (var ini = IniFile.FromFile(conf))
+            {                
+                ini.WriteValue("", "GameType", gameExtension);
+                ini.WriteValue("", "Fullscreen", fullscreen ? "1" : "0");
+                ini.WriteValue("", "GamePath", gamePath);
+                ini.WriteValue("", "SavePath", savesPath);
+                ini.WriteValue("", "AudioDriver", "openal");
+                ini.WriteValue("", "CapFPS", "0");
+
+                if (SystemConfig.isOptSet("resolution") && !string.IsNullOrEmpty(SystemConfig["resolution"]))
+                {                                       
+
+                    if (SystemConfig["resolution"].ToLower().Contains("x"))
+                    {
+                        string[] gameResolution = SystemConfig["resolution"].ToLower().Split('x');
+                        ini.WriteValue("", "Width", gameResolution[0]);
+                        ini.WriteValue("", "Height", gameResolution[1]);
+
+                    }
+                    else if (SystemConfig["resolution"].ToLower() == "desktop")
+                    {
+                        ini.WriteValue("", "Width", (resolution == null ? Screen.PrimaryScreen.Bounds.Width : resolution.Width).ToString());
+                        ini.WriteValue("", "Height", (resolution == null ? Screen.PrimaryScreen.Bounds.Height : resolution.Height).ToString());
+                    }
+                    else
+                    {
+                        ini.WriteValue("", "Width", "640");
+                        ini.WriteValue("", "Height", "480");
+                    }
+                }
+                else
+                {
+                    ini.WriteValue("", "Width", "640");
+                    ini.WriteValue("", "Height", "480");
+                }
+
+                BindIniFeature(ini, "", "Logging", "Logging", "0");
+                BindIniFeature(ini, "", "SkipIntroVideos", "SkipIntroVideos", "0");
+                BindIniFeature(ini, "", "DrawFPS", "DrawFPS", "0");
+                BindIniFeature(ini, "", "EnableCheatKeys", "EnableCheatKeys", "0");
+                BindIniFeature(ini, "", "DebugMode", "DebugMode", "0");
+                BindIniFeature(ini, "", "Encoding", "Encoding", "default");
+                BindIniFeature(ini, "", "Bpp", "Bpp", "32");
+                BindIniFeature(ini, "", "DebugMode", "DebugMode", "0");
+                BindIniFeature(ini, "", "SpriteFogOfWar", "SpriteFogOfWar", "0");
+
+            }
+
+        }
+
+        public override int RunAndWait(ProcessStartInfo path)
+        {
+            int ret = base.RunAndWait(path);
+            return ret;
+        }
+    }
+}
