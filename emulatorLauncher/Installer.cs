3d70177bd56245be6f4db043fbb738c30a65d73c
Benoit S <tartifless@gmail.com>
2024-04-04 23:27:34 +0200

    Fix installer

--- emulatorLauncher/Installer.cs
+++ emulatorLauncher/Installer.cs
@@ -98,8 +98,8 @@ namespace EmulatorLauncher
             { new Installer("gemrb", "gemrb", "gemrb.exe") },
             { new Installer("psxmame", "psxmame", "mame.exe") },
             { new Installer("fbneo", "fbneo", "fbneo64.exe") },
-            { new Installer("sonic3air", "sonic3air", "Sonic3AIR.exe") }
-            { new Installer("devilutionx", "devilutionx", "devilutionx.exe") },
+            { new Installer("sonic3air", "sonic3air", "Sonic3AIR.exe") },
+            { new Installer("devilutionx", "devilutionx", "devilutionx.exe") }
         };
 
         static List<string>noVersionExe = new List<string>()
