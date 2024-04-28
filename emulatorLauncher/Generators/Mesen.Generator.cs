bf1dd93fd461f011766bafdb134ded08c96799f8
Benoit S. <tartifless@gmail.com>
2024-04-28 22:54:58 +0200

    Add supergrafx to mesen

--- emulatorLauncher/Generators/Mesen.Generator.cs
+++ emulatorLauncher/Generators/Mesen.Generator.cs
@@ -290,6 +290,7 @@ namespace EmulatorLauncher
                 case "sgb":
                     return "Gameboy";
                 case "pcengine":
+                case "supergrafx":
                     return "PcEngine";
             }
             return "none";
