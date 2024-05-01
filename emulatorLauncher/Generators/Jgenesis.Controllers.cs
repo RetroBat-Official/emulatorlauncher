4537978d388bf797e23e4baef5bc5ce31052e6b2
Benoit S. <tartifless@gmail.com>
2024-05-01 02:20:59 +0200

    Add Jgenesis emulator

---
+++ emulatorLauncher/Generators/Jgenesis.Controllers.cs
@@ -0,0 +1,179 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using EmulatorLauncher.Common.EmulationStation;
+
+namespace EmulatorLauncher
+{
+    partial class JgenesisGenerator : Generator
+    {
+        private void SetupControllers(string jgenSystem)
+        {
+            if (Program.SystemConfig.isOptSet("disableautocontrollers") && Program.SystemConfig["disableautocontrollers"] == "1")
+                return;
+
+            if (jgenSystem == "sega_cd")
+                jgenSystem = "genesis";
+
+            int maxPad = 1;
+            if (systemMaxPad.ContainsKey(jgenSystem))
+                maxPad = systemMaxPad[jgenSystem];
+
+            // Inject controllers                
+            foreach (var controller in this.Controllers.OrderBy(i => i.PlayerIndex).Take(maxPad))
+                ConfigureInput(controller, controller.PlayerIndex, jgenSystem);
+        }
+
+        private void ConfigureInput(Controller controller, int playerIndex, string jgenSystem)
+        {
+            if (controller == null || controller.Config == null)
+                return;
+
+            if (controller.IsKeyboard)
+                ConfigureKeyboard(controller.Config, jgenSystem);
+            else
+                ConfigureJoystick(controller, playerIndex, jgenSystem);
+        }
+
+        private void ConfigureJoystick(Controller ctrl, int playerIndex, string jgenSystem)
+        {
+            if (ctrl == null)
+                return;
+
+            InputConfig joy = ctrl.Config;
+            if (joy == null)
+                return;
+
+            int index = ctrl.SdlController != null ? ctrl.SdlController.Index : ctrl.DeviceIndex;
+
+            bool revertButtons = SystemConfig.isOptSet("jgen_revertbuttons") && SystemConfig.getOptBoolean("jgen_revertbuttons");
+            bool isXInput = ctrl.IsXInputDevice;
+
+            if (playerIndex == 1)
+                WriteHotkeys(index, isXInput);
+        }
+
+        private void ConfigureKeyboard(InputConfig keyboard, string jgenSystem)
+        {
+            if (keyboard == null)
+                return;
+
+        }
+
+        private void WriteHotkeys(int index, bool isXInput)
+        {
+
+        }
+
+        static readonly Dictionary<string, int> systemMaxPad = new Dictionary<string, int>()
+        {
+            { "smsgg", 2 },
+            { "genesis", 2 },
+            { "game_boy", 1 },
+            { "nes", 2 },
+            { "snes", 2 }
+        };
+
+        private static string SdlToKeyCode(long sdlCode)
+        {
+
+            //The following list of keys has been verified, ryujinx will not allow wrong string so do not add a key until the description has been tested in the emulator first
+            switch (sdlCode)
+            {
+                case 0x0D: return "6";      // ENTER
+                case 0x08: return "2";      // Backspace
+                case 0x09: return "3";      // TAB
+                case 0x20: return "18";     // SPACE
+                case 0x2B: return "0";      // Plus
+                case 0x2C: return "142";    // Comma
+                case 0x2D: return "0";      // Minus
+                case 0x2E: return "144";    // Period
+                case 0x2F: return "145";    // Slash
+                case 0x30: return "34";     // Number 0
+                case 0x31: return "35";
+                case 0x32: return "36";
+                case 0x33: return "37";
+                case 0x34: return "38";
+                case 0x35: return "39";
+                case 0x36: return "40";
+                case 0x37: return "41";
+                case 0x38: return "42";
+                case 0x39: return "43";     // Number 9
+                case 0x3B: return "140";    // Semi column
+                case 0x61: return "44";     // A
+                case 0x62: return "45";
+                case 0x63: return "46";
+                case 0x64: return "47";
+                case 0x65: return "48";
+                case 0x66: return "49";
+                case 0x67: return "50";
+                case 0x68: return "51";
+                case 0x69: return "52";
+                case 0x6A: return "53";
+                case 0x6B: return "54";
+                case 0x6C: return "55";
+                case 0x6D: return "56";
+                case 0x6E: return "57";
+                case 0x6F: return "58";
+                case 0x70: return "59";
+                case 0x71: return "60";
+                case 0x72: return "61";
+                case 0x73: return "62";
+                case 0x74: return "63";
+                case 0x75: return "64";
+                case 0x76: return "65";
+                case 0x77: return "66";
+                case 0x78: return "67";
+                case 0x79: return "68";
+                case 0x7A: return "69";             // Z
+                case 0x7F: return "32";             // Delete
+                case 0x4000003A: return "90";       // F1
+                case 0x4000003B: return "91";
+                case 0x4000003C: return "92";
+                case 0x4000003D: return "93";
+                case 0x4000003E: return "94";
+                case 0x4000003F: return "95";
+                case 0x40000040: return "96";
+                case 0x40000041: return "97";
+                case 0x40000042: return "98";
+                case 0x40000043: return "99";
+                case 0x40000044: return "100";
+                case 0x40000045: return "101";      // F12
+                case 0x40000047: return "0";        // Scrolllock
+                case 0x40000048: return "0";        // Pause
+                case 0x40000049: return "31";       // Insert
+                case 0x4000004A: return "22";       // Home
+                case 0x4000004B: return "19";       // PageUp
+                case 0x4000004D: return "21";       // End
+                case 0x4000004E: return "20";       // Page Down
+                case 0x4000004F: return "25";       // Right  
+                case 0x40000050: return "23";       // Left
+                case 0x40000051: return "26";       // Down
+                case 0x40000052: return "24";       // Up
+                case 0x40000053: return "114";      // Numlock  
+                case 0x40000054: return "89";       // KeypadDivide
+                case 0x40000055: return "84";       // KeypadMultiply
+                case 0x40000056: return "87";       // KeypadSubtract
+                case 0x40000057: return "85";       // KeypadAdd
+                case 0x40000058: return "6";        // Enter
+                case 0x40000059: return "75";       // Numpad 1
+                case 0x4000005A: return "76";
+                case 0x4000005B: return "77";
+                case 0x4000005C: return "78";
+                case 0x4000005D: return "79";
+                case 0x4000005E: return "80";
+                case 0x4000005F: return "81";
+                case 0x40000060: return "82";
+                case 0x40000061: return "83";
+                case 0x40000062: return "74";       // Numpad 0
+                case 0x40000063: return "88";       // KeypadDecimal
+                case 0x40000085: return "88";
+                case 0x400000E0: return "118";      // Left control
+                case 0x400000E1: return "116";      // Left shift
+                case 0x400000E2: return "120";      // Left ALT
+                case 0x400000E4: return "119";      // Right control
+                case 0x400000E5: return "117";      // Right shift
+            }
+            return "0";
+        }
+    }
+}
---
+++ emulatorLauncher/Generators/Jgenesis.Generator.cs
@@ -0,0 +1,246 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Diagnostics;
+using EmulatorLauncher.Common.FileFormats;
+using EmulatorLauncher.Common;
+using EmulatorLauncher.Common.Compression;
+using System.Linq;
+
+namespace EmulatorLauncher
+{
+    partial class JgenesisGenerator : Generator
+    {
+        private BezelFiles _bezelFileInfo;
+        private ScreenResolution _resolution;
+        private string _destRom;
+
+        public override System.Diagnostics.ProcessStartInfo Generate(string system, string emulator, string core, string rom, string playersControllers, ScreenResolution resolution)
+        {
+            string path = AppConfig.GetFullPath("jgenesis");
+
+            string exe = Path.Combine(path, "jgenesis-gui.exe");
+            if (!File.Exists(exe))
+                return null;
+
+            bool fullscreen = !IsEmulationStationWindowed() || SystemConfig.getOptBoolean("forcefullscreen");
+
+            if (Path.GetExtension(rom).ToLowerInvariant() == ".zip")
+            {
+                string romFile = null;
+                var entries = Zip.ListEntries(rom).Where(e => !e.IsDirectory).Select(e => e.Filename).ToArray();
+
+                if (entries.Length == 0)
+                    return null;
+
+                romFile = entries[0];
+
+                if (romFile == null)
+                    return null;
+
+                _destRom = Path.Combine(Path.GetTempPath(), Path.GetFileName(romFile));
+
+                Zip.Extract(rom, Path.GetTempPath());
+                if (!File.Exists(_destRom))
+                    return null;
+
+                rom = _destRom;
+            }
+
+            // settings (toml configuration)
+            SetupTomlConfiguration(path, system, rom, fullscreen);
+
+            _bezelFileInfo = BezelFiles.GetBezelFiles(system, rom, resolution);
+            _resolution = resolution;
+            
+
+            // command line parameters
+            var commandArray = new List<string>
+            {
+                "-f",
+                "\"" + rom + "\""
+            };
+
+            string args = string.Join(" ", commandArray);
+
+            return new ProcessStartInfo()
+            {
+                FileName = exe,
+                WorkingDirectory = path,
+                Arguments = args,
+            };
+        }
+
+        private void SetupTomlConfiguration(string path, string system, string rom, bool fullscreen)
+        {
+            string settingsFile = Path.Combine(path, "jgenesis-config.toml");
+
+            using (IniFile ini = new IniFile(settingsFile, IniOptions.KeepEmptyLines | IniOptions.UseSpaces))
+            {
+
+                string jgenSystem = GetJgenesisSystem(system);
+                if (jgenSystem == null)
+                    return;
+
+                ini.WriteValue("common", "launch_in_fullscreen", fullscreen ? "true" : "false");
+                BindIniFeature(ini, "common", "wgpu_backend", "jgen_renderer", "Auto");
+                BindIniFeature(ini, "common", "vsync_mode", "jgen_vsync", "Enabled");
+                BindBoolIniFeature(ini, "common", "force_integer_height_scaling", "integerscale", "true", "false");
+                BindIniFeature(ini, "common", "filter_mode", "jgen_filter", "Linear");
+                BindIniFeature(ini, "common", "preprocess_shader", "jgen_shader", "None");
+
+                ConfigureSMS(ini, jgenSystem);
+                ConfigureGenesis(ini, jgenSystem);
+                ConfigureNes(ini, jgenSystem);
+                ConfigureSnes(ini, jgenSystem);
+                ConfigureGameboy(ini, jgenSystem);
+
+                SetupControllers(jgenSystem);
+
+                // Save toml file
+                ini.Save();
+            }
+        }
+
+        private void ConfigureNes(IniFile ini, string system)
+        {
+            if (system != "nes")
+                return;
+
+            BindIniFeature(ini, "nes", "forced_timing_mode", "jgen_nes_timing", "Auto");
+            BindIniFeature(ini, "nes", "aspect_ratio", "jgen_nes_ratio", "Ntsc");
+            BindBoolIniFeature(ini, "nes", "remove_sprite_limit", "jgen_spritelimit", "true", "false");
+            BindBoolIniFeature(ini, "nes", "pal_black_border", "jgen_nes_palborder", "true", "false");
+            BindBoolIniFeature(ini, "nes", "audio_60hz_hack", "jgen_nes_audiohack", "false", "true");
+        }
+
+        private void ConfigureSMS(IniFile ini, string system)
+        {
+            if (system != "smsgg")
+                return;
+
+            BindBoolIniFeature(ini, "smsgg", "remove_sprite_limit", "jgen_spritelimit", "true", "false");
+            BindIniFeature(ini, "smsgg", "sms_aspect_ratio", "jgen_sms_ratio", "Ntsc");
+            BindIniFeature(ini, "smsgg", "gg_aspect_ratio", "jgen_gg_ratio", "GgLcd");
+            BindIniFeature(ini, "smsgg", "sms_region", "jgen_sms_region", "International");
+            BindIniFeature(ini, "smsgg", "sms_timing_mode", "jgen_sms_timing", "Ntsc");
+            BindIniFeature(ini, "smsgg", "sms_model", "jgen_sms_model", "Sms2");
+            BindBoolIniFeature(ini, "smsgg", "fm_sound_unit_enabled", "jgen_sms_fmchip", "false", "true");
+        }
+
+        private void ConfigureGenesis(IniFile ini, string system)
+        {
+            if (system != "genesis" && system != "sega_cd")
+                return;
+
+            BindIniFeature(ini, "genesis", "forced_region", "jgen_genesis_region", "Auto");
+            BindIniFeature(ini, "genesis", "forced_timing_mode", "jgen_genesis_timing", "Auto");
+            BindBoolIniFeature(ini, "genesis", "remove_sprite_limits", "jgen_spritelimit", "true", "false");
+            BindIniFeature(ini, "genesis", "aspect_ratio", "jgen_genesis_ratio", "Ntsc");
+
+            if (SystemConfig["jgen_genesis_pad"] == "3btn")
+            {
+                ini.WriteValue("inputs", "genesis_p1_type", "ThreeButton");
+                ini.WriteValue("inputs", "genesis_p2_type", "ThreeButton");
+            }
+            else
+            {
+                ini.WriteValue("inputs", "genesis_p1_type", "SixButton");
+                ini.WriteValue("inputs", "genesis_p2_type", "SixButton");
+            }
+
+            if (system == "sega_cd")
+            {
+                string regionbios = "bios_CD_U.bin";
+                if (SystemConfig.isOptSet("jgen_genesis_region") && !string.IsNullOrEmpty(SystemConfig["jgen_genesis_region"]))
+                    regionbios = SystemConfig["jgen_genesis_region"];
+
+                string segaCdBios = Path.Combine(AppConfig.GetFullPath("bios"), regionbios);
+
+                ini.WriteValue("sega_cd", "bios_path", "'" + segaCdBios + "'");
+                BindBoolIniFeature(ini, "sega_cd", "enable_ram_cartridge", "jgen_segacd_ramcart", "false", "true");
+                BindBoolIniFeature(ini, "sega_cd", "load_disc_into_ram", "jgen_segacd_loadtoram", "true", "false");
+            }   
+        }
+
+
+        private void ConfigureGameboy(IniFile ini, string system)
+        {
+            if (system != "game_boy")
+                return;
+
+            BindIniFeature(ini, "game_boy", "gb_palette", "jgen_gb_palette", "GreenTint");
+            BindIniFeature(ini, "game_boy", "aspect_ratio", "jgen_gb_ratio", "SquarePixels");
+            BindBoolIniFeature(ini, "game_boy", "force_dmg_mode", "jgen_gb_dmg", "true", "false");
+            BindBoolIniFeature(ini, "game_boy", "pretend_to_be_gba", "jgen_gb_gba", "true", "false");
+            BindIniFeature(ini, "game_boy", "gbc_color_correction", "jgen_gb_colorcorrect", "GbcLcd");
+            BindBoolIniFeature(ini, "game_boy", "audio_60hz_hack", "jgen_gb_60fps", "true", "false");
+        }
+
+        private void ConfigureSnes(IniFile ini, string system)
+        {
+            if (system != "snes")
+                return;
+
+            BindIniFeature(ini, "snes", "forced_timing_mode", "jgen_snes_timing", "Auto");
+            BindIniFeature(ini, "snes", "aspect_ratio", "jgen_snes_ratio", "Ntsc");
+            BindBoolIniFeature(ini, "snes", "audio_60hz_hack", "jgen_snes_audiohack", "false", "true");
+            BindIniFeature(ini, "snes", "gsu_overclock_factor", "jgen_snes_superfx_overclock", "1");
+
+            SetupGuns(ini, system);
+        }
+
+        private void SetupGuns(IniFile ini, string jgenSystem)
+        {
+            if (!SystemConfig.getOptBoolean("use_guns"))
+                return;
+
+            if (jgenSystem == "snes")
+            {
+                ini.WriteValue("inputs", "snes_p2_type", "SuperScope");
+
+                ini.WriteValue("inputs.snes_keyboard.super_scope", "fire", "MouseLeft");
+                ini.WriteValue("inputs.snes_keyboard.super_scope", "cursor", "MouseRight");
+                ini.WriteValue("inputs.snes_keyboard.super_scope", "pause", "MouseMiddle");
+            }
+        }
+
+        private string GetJgenesisSystem(string System)
+        {
+            switch (System)
+            {
+                case "nes":
+                    return "nes";
+                case "snes":
+                    return "snes";
+                case "segacd":
+                    return "sega_cd";
+                case "megadrive":
+                    return "genesis";
+                case "mastersystem":
+                case "gamegear":
+                    return "smsgg";
+                case "gb":
+                case "gbc":
+                    return "game_boy";
+            }
+            return null;
+        }
+
+        public override int RunAndWait(ProcessStartInfo path)
+        {
+            FakeBezelFrm bezel = null;
+
+            if (_bezelFileInfo != null)
+                bezel = _bezelFileInfo.ShowFakeBezel(_resolution);
+
+            int ret = base.RunAndWait(path);
+
+            bezel?.Dispose();
+
+            if (ret == 1)
+                return 0;
+
+            return ret;
+        }
+    }
+}
