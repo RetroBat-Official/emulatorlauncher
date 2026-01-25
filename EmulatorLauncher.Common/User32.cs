73fcb5025d636fed3bb2940b9381b989921dd19d
Fabrice CARUSO <fabrice.caruso@free.fr>
2026-01-25 20:38:45 +0100

    [GameCenter] Try to pause emulator that don't pause when loosing focus with NtSuspendProcess + Enhancements

--- EmulatorLauncher.Common/User32.cs
+++ EmulatorLauncher.Common/User32.cs
@@ -124,16 +124,6 @@ namespace EmulatorLauncher.Common
         [DllImport("user32.dll")]
         public static extern IntPtr GetProp(IntPtr hWnd, string lpString);
 
-        public enum GW : int
-        {
-            HWNDFIRST = 0,
-            HWNDLAST = 1,
-            HWNDNEXT = 2,
-            HWNDPREV = 3,
-            OWNER = 4,
-            CHILD = 5
-        }
-
         [DllImport("user32.dll", SetLastError = true)]
         public static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
 
@@ -279,6 +269,24 @@ namespace EmulatorLauncher.Common
         {
             return AttachConsole(-1);
         }
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern IntPtr OpenProcess(ProcessAccessFlags processAccess, bool bInheritHandle, uint processId);
+
+        [DllImport("kernel32.dll", ExactSpelling = true, SetLastError = true)]        
+        public static extern bool CloseHandle(IntPtr hObject);
+
+        [DllImport("ntdll.dll", SetLastError = false)]
+        public static extern IntPtr NtSuspendProcess(IntPtr processHandle);
+
+        [DllImport("ntdll.dll", SetLastError = true)]
+        public static extern IntPtr NtResumeProcess(IntPtr processHandle);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool DebugActiveProcess(uint dwProcessId);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool DebugActiveProcessStop(uint dwProcessId);
 
         [DllImport("kernel32.dll")]
         public static extern uint GetCurrentThreadId();
@@ -453,6 +461,16 @@ namespace EmulatorLauncher.Common
         public byte AlphaFormat;
     }
 
+    public enum GW : int
+    {
+        HWNDFIRST = 0,
+        HWNDLAST = 1,
+        HWNDNEXT = 2,
+        HWNDPREV = 3,
+        OWNER = 4,
+        CHILD = 5
+    }
+
     public enum GWL : int
     {
         WNDPROC = -4,
@@ -462,6 +480,24 @@ namespace EmulatorLauncher.Common
         EXSTYLE = -20,
         USERDATA = -21,
         ID = -12
+    }
+
+    [Flags]
+    public enum ProcessAccessFlags : uint
+    {
+        All = 0x001F0FFF,
+        Terminate = 0x00000001,
+        CreateThread = 0x00000002,
+        VirtualMemoryOperation = 0x00000008,
+        VirtualMemoryRead = 0x00000010,
+        VirtualMemoryWrite = 0x00000020,
+        DuplicateHandle = 0x00000040,
+        CreateProcess = 0x000000080,
+        SetQuota = 0x00000100,
+        SetInformation = 0x00000200,
+        QueryInformation = 0x00000400,
+        QueryLimitedInformation = 0x00001000,
+        Synchronize = 0x00100000
     }
 
     [Flags]
