03ae99e16009c6251d453140c52ed89da53b635b
Fabrice CARUSO <fabrice.caruso@free.fr>
2026-02-14 16:42:33 +0100

    [GameCenter] Use BLURBEHIND for borderless games

--- emulatorLauncher/ControlCenter/OverlayForm.cs
+++ emulatorLauncher/ControlCenter/OverlayForm.cs
@@ -1,5 +1,4 @@
-﻿using EmulatorLauncher.Common;
-using System;
+﻿using System;
 using System.Drawing;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
@@ -44,8 +43,18 @@ namespace EmulatorLauncher.ControlCenter
 
             if (!ExclusiveFullScreen)
             {
-                Opacity = 0.7;
-                
+                try
+                {
+                    this.BackColor = Color.Fuchsia;
+                    this.TransparencyKey = this.BackColor;
+                    EnableFluent();
+                }
+                catch 
+                {                    
+                    this.BackColor = Color.Black;
+                    Opacity = 0.7;
+                }
+
                 if (_background != null)
                 {
                     _background.Dispose();
@@ -106,6 +115,63 @@ namespace EmulatorLauncher.ControlCenter
 
             base.WndProc(ref m);
         }
-    }
 
+        #region Fluent
+        [DllImport("user32.dll")]
+        static extern int SetWindowCompositionAttribute(IntPtr hwnd, ref WindowCompositionAttributeData data);
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct AccentPolicy
+        {
+            public AccentState AccentState; // 3 = BlurBehind, 4 = Acrylic
+            public int AccentFlags;
+            public uint GradientColor; 
+            public int AnimationId;
+        }
+
+        enum AccentState
+        {
+            ACCENT_DISABLED = 0,
+            ACCENT_ENABLE_GRADIENT = 1,
+            ACCENT_ENABLE_TRANSPARENTGRADIENT = 2,
+            ACCENT_ENABLE_BLURBEHIND = 3,
+            ACCENT_ENABLE_ACRYLICBLURBEHIND = 4,
+            ACCENT_INVALID_STATE = 5
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct WindowCompositionAttributeData
+        {
+            public WindowCompositionAttribute Attribute;
+            public IntPtr Data;
+            public int SizeOfData;
+        }
+
+        enum WindowCompositionAttribute
+        {
+            WCA_ACCENT_POLICY = 19
+        }
+
+        void EnableFluent()
+        {
+            var accent = new AccentPolicy();
+            accent.AccentState = AccentState.ACCENT_ENABLE_BLURBEHIND;
+            accent.GradientColor = 0xC0000000;
+            accent.AccentFlags = 3;
+
+            var accentStructSize = Marshal.SizeOf(accent);
+            var accentPtr = Marshal.AllocHGlobal(accentStructSize);
+            Marshal.StructureToPtr(accent, accentPtr, false);
+
+            var data = new WindowCompositionAttributeData();
+            data.Attribute = WindowCompositionAttribute.WCA_ACCENT_POLICY;
+            data.SizeOfData = accentStructSize;
+            data.Data = accentPtr;
+
+            SetWindowCompositionAttribute(this.Handle, ref data);
+
+            Marshal.FreeHGlobal(accentPtr);
+        }
+        #endregion
+    }
 }
