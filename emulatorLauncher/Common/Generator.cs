1e57929b082fe4e3ec49116a8b1c2737226aca67
Alexandre Derumier <aderumier@gmail.com>
2026-03-20 01:26:03 +0100

    add sgb-msu1 squashfs support

--- emulatorLauncher/Common/Generator.cs
+++ emulatorLauncher/Common/Generator.cs
@@ -335,6 +335,7 @@ namespace EmulatorLauncher
             string[] saturnExtensions = new string[] { ".m3u", ".chd", ".cue", ".iso", ".ccd", ".mds", ".toc" };
             string[] scummVMExtensions = new string[] { ".scummvm" };
             string[] snesMSUExtensions = new string[] { ".sfc", ".smc" };
+            string[] sgbMSUExtensions = new string[] { ".gb", ".gbc" };
             string[] extensions = new string[] { };
             bool tryMount = false;
             bool silent = false;
@@ -367,6 +368,11 @@ namespace EmulatorLauncher
             else if (system == "snes-msu1")
             {
                 extensions = snesMSUExtensions;
+                tryMount = true;
+            }
+            else if (system == "sgb-msu1")
+            {
+                extensions = sgbMSUExtensions;
                 tryMount = true;
             }
 
